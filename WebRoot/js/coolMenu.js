51a62828f18d555cca5e331e740e373e86dd1c0a
manfreddrathen <manfreddrathen@0ae29167-c2e2-675a-246c-068797d430c6>
2013-04-16 06:45:48 +0000

    manfred update

---
+++ WebRoot/js/coolMenu.js
@@ -0,0 +1,17 @@
+//Our coolMenu jquery plugin for automatic menu creation
+//Author:Xuezi Zhang
+//Version:1.0
+(function($){
+    $.fn.coolMenu = function(config){
+        //各种属性、参数
+    };
+    var config = $.extend(defaults, config);
+    
+    function drawlayout(){
+    	
+    }
+    
+    function initiateMenu(){
+    	
+    }
+})(jQuery);
---
+++ src/org/manfred/dao/MenuDaoImp.java
@@ -0,0 +1,18 @@
+package org.manfred.dao;
+
+import org.gaara.hibernate.HibernateSessionFactory;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+public class MenuDaoImp extends BasicCURD{
+	public MenuDaoImp(){
+		super();
+		System.out.println("MenuDaoImp In----------!");
+	}
+	
+	//private Connection connection = null;
+}
